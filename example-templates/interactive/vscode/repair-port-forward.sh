e9be78679723acb5f0ef07421822b46b7de57acf
Benoit Jacob <jacob.benoit.1@gmail.com>
2025-11-25 20:57:22 -0500

    also recreate the port-forward if it wasnt running anymore
    Signed-off-by: Benoit Jacob <jacob.benoit.1@gmail.com>

--- example-templates/interactive/vscode/repair-port-forward.sh
+++ example-templates/interactive/vscode/repair-port-forward.sh
@@ -5,17 +5,34 @@ pattern="kubectl port-forward"
 
 ps_entry="$(ps a | (grep -F "${pattern}" || :) | (grep -v grep || :))"
 
-if [[ -z "$ps_entry" ]]
+if [[ -n "$ps_entry" ]]
 then
-  echo "No process matches \"$pattern\"."
-  exit 1
+  echo "Found existing process: $ps_entry"
+  pid="$(echo "$ps_entry" | grep -o '^[[:space:]]*[0-9]\+')"
+  command="$(echo "$ps_entry" | grep -o 'kubectl.*')"
+  echo "Killing $pid..."
+  kill $pid
+  echo "Rerunning command $command..."
+  $command
+  exit 0
 fi
 
-pid="$(echo $ps_entry | grep -o '^[[:space:]]*[0-9]\+')"
-command="$(echo $ps_entry | grep -o 'kubectl.*')"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+CONFIG_FILE="${SCRIPT_DIR}/config.json"
+NAMESPACE=$(jq -r '.namespace' "$CONFIG_FILE")
 
-echo "Killing $pid..."
-kill $pid
+get_pods_entry="$(kubectl get pods -n "$NAMESPACE" | (grep "\bRunning\b" || :))"
 
-echo "Rerunning command $command..."
-$command
+if [[ -n "$get_pods_entry" ]]
+then
+  echo "Found running pod: $get_pods_entry"
+  pod_name="$(echo "$get_pods_entry" | grep -o '^[^[:space:]]\+')"
+  echo "Pod name: $pod_name"
+  command="kubectl port-forward -n $NAMESPACE $pod_name 2222:22"
+  echo "Executing: $command"
+  $command
+  exit 0
+fi
+
+echo "No running pod."
+exit 1
